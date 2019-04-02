478d735b12167ed72b99a4a8f28a47d1fd62860b
Destin Moulton <destin.moulton@gmail.com>
2019-04-02 08:15:45 -0600

    Added gulp sass compilation; moved css into new sass file

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
-var hoganCompiler = require("gulp-hogan-precompile");
 var declare = require("gulp-declare");
 var uglify = require("gulp-uglify");
 var header = require("gulp-header");
 const rename = require("gulp-rename");
 const browserify = require("gulp-browserify");
+const sass = require("gulp-sass");
 
-gulp.task("compile-coffee", function() {
+var pkg = require("./package.json");
+var banner = [
+    "/**",
+    " * <%= pkg.name %> - <%= pkg.description %>",
+    " * @author <%= pkg.author %>",
+    " * @version v<%= pkg.version %>",
+    " * @link <%= pkg.homepage %>",
+    " * @license <%= pkg.license %>",
+    " */",
+    ""
+].join("\n");
+
+gulp.task("compile:coffee", function() {
     return gulp
         .src("src/WolfCage.coffee", { read: false })
         .pipe(
@@ -21,49 +33,27 @@ gulp.task("compile-coffee", function() {
         .pipe(gulp.dest("dist/"));
 });
 
-gulp.task("compile-templates", function() {
+gulp.task("compile:sass", function() {
     return gulp
-        .src("templates/**/*.mustache")
-        .pipe(hoganCompiler())
-        .pipe(
-            declare({
-                namespace: "templates",
-                noRedeclare: true
-            })
-        )
-        .pipe(concat("wolfcage.templates.js"))
-        .pipe(gulp.dest("./dist"));
+        .src("src/wolfcage.scss")
+        .pipe(sass())
+        .pipe(header(banner, { pkg }))
+        .pipe(gulp.dest("dist/"));
 });
 
-gulp.task("watch-coffee", function() {
+gulp.task("watch", function() {
+    gulp.watch(
+        "src/wolfcage.scss",
+        { ignoreInitial: false },
+        gulp.series(["compile:sass"])
+    );
+
     gulp.watch(
         "src/*.coffee",
         { ignoreInitial: false },
-        gulp.series(["compile-coffee"])
+        gulp.series(["compile:coffee"])
     );
 });
-
-gulp.task("watch-templates", function() {
-    gulp.start("compile-templates");
-    gulp.watch("templates/*.mustache", ["compile-templates"]);
-});
-
-gulp.task("watch-both", function() {
-    gulp.start("watch-coffee");
-    gulp.start("watch-templates");
-});
-
-var pkg = require("./package.json");
-var banner = [
-    "/**",
-    " * <%= pkg.name %> - <%= pkg.description %>",
-    " * @author <%= pkg.author %>",
-    " * @version v<%= pkg.version %>",
-    " * @link <%= pkg.homepage %>",
-    " * @license <%= pkg.license %>",
-    " */",
-    ""
-].join("\n");
 
 gulp.task("uglify-js", function() {
     gulp.src(["dist/wolfcage.templates.js", "dist/wolfcage.js"])
